a293600aa950542da06e36a4b0938e1a2086073a
diegocastro-ac <diegocastroac123@gmail.com>
2026-08-14 16:24:39 -0500

    feat: add exercises to training routines

---
+++ Application/Services/ExerciseService.cs
@@ -0,0 +1,60 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Enums;
+
+namespace Application.Services
+{
+    public class ExerciseService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ExerciseService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Exercise? AddStrengthExercise(Guid userId, Guid routineId, string name, MuscleGroup muscleGroup,int sets, int repetitions, decimal weight)
+        {
+            var routine = FindRoutine(userId, routineId);
+
+            if (routine is null)
+            {
+                return null;
+            }
+
+            var exercise = new StrengthExercise(Guid.NewGuid(), name, muscleGroup, sets, repetitions, weight);
+
+            routine.AddExercise(exercise);
+
+            return exercise;
+        }
+
+        public Exercise? AddCardioExercise(Guid userId, Guid routineId, string name, MuscleGroup muscleGroup, TimeSpan duration, decimal distance)
+        {
+            var routine = FindRoutine(userId, routineId);
+
+            if (routine is null)
+            {
+                return null;
+            }
+
+            var exercise = new CardioExercise(Guid.NewGuid(), name, muscleGroup, duration, distance);
+
+            routine.AddExercise(exercise);
+
+            return exercise;
+        }
+
+        private Routine? FindRoutine(Guid userId, Guid routineId)
+        {
+            var user = _userRepository.GetById(userId);
+
+            if (user is null)
+            {
+                return null;
+            }
+
+            return user.Routines.FirstOrDefault(routine => routine.Id == routineId);
+        }
+    }
+}
--- Console/Program.cs
+++ Console/Program.cs
@@ -6,10 +6,60 @@ var userRepository = new InMemoryUserRepository();
 
 var userService = new UserService(userRepository);
 var routineService = new RoutineService(userRepository);
+var exerciseService = new ExerciseService(userRepository);
 
 var user = userService.CreateUser("Diego");
 
-var routine = routineService.CreateRoutine(user.Id, "Full Body", TrainingGoal.GeneralFitness);
+var routine = routineService.CreateRoutine(
+    user.Id,
+    "Full Body",
+    TrainingGoal.GeneralFitness);
+
+if (routine is null)
+{
+    return;
+}
+
+exerciseService.AddStrengthExercise(
+    user.Id,
+    routine.Id,
+    "Squat",
+    MuscleGroup.Legs,
+    3,
+    10,
+    60);
+
+exerciseService.AddStrengthExercise(
+    user.Id,
+    routine.Id,
+    "Bench Press",
+    MuscleGroup.Chest,
+    3,
+    10,
+    50);
+
+exerciseService.AddCardioExercise(
+    user.Id,
+    routine.Id,
+    "Running",
+    MuscleGroup.Legs,
+    TimeSpan.FromMinutes(20),
+    3);
+
+//exerciseService.AddStrengthExercise(
+//    user.Id,
+//    routine.Id,
+//    "Invalid Exercise",
+//    MuscleGroup.Legs,
+//    -3,
+//    10,
+//    60);
 
 Console.WriteLine($"User: {user.Name}");
-Console.WriteLine($"Routine: {routine?.Name}");
+Console.WriteLine($"Routine: {routine.Name}");
+Console.WriteLine();
+
+foreach (var exercise in routine.Exercises)
+{
+    Console.WriteLine(exercise.Execute());
+}
